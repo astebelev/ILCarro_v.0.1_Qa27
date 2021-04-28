3201dee77b82888f62016abaaa068971ba58e5c0
AntonStebelev <astebelev@gmail.com>
2021-04-28 13:57:32 +0300

    added user and app

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,22 +1,19 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import manager.ApplicationManager;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
-import java.util.concurrent.TimeUnit;
-
 public class TestBase {
-    WebDriver wd;
+
+    protected  static ApplicationManager app= new ApplicationManager();
 
     @BeforeSuite
-    public void init(){
-        wd=new ChromeDriver();
-        wd.manage().window().maximize();
-        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        wd.navigate().to("https://ilcarro-dev-v1.firebaseapp.com/");
+    public void setUp() {
+        app.init();
     }
     @AfterSuite
     public void tearDown(){
-        wd.quit();
+        app.stop();
     }
+
+
 }
