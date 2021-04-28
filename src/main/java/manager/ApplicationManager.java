8418c59737e237218f4d74076f2d36cd00acb2cb
AntonStebelev <astebelev@gmail.com>
2021-04-28 14:01:47 +0300

    login test

---
+++ src/main/java/manager/ApplicationManager.java
@@ -0,0 +1,28 @@
+package manager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+    static WebDriver wd;
+    UserHelper user;
+
+    public UserHelper user() {
+        return user;
+    }
+
+
+    public void init() {
+        wd = new ChromeDriver();
+        wd.manage().window().maximize();
+        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wd.navigate().to("https://ilcarro.xyz/search");
+        user = new UserHelper(wd);
+    }
+
+    public void stop() {
+        wd.quit();
+    }
+}
